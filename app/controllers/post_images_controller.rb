1f749ebe737ce658026c9e9810085e72ea64bbfd
EC2 Default User <ec2-user@ip-172-31-19-56.ap-southeast-2.compute.internal>
2023-10-17 23:57:15 +0000

    [finish]Setting Bootstrap

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -14,8 +14,8 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
-    
+    # @post_images = PostImage.
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
